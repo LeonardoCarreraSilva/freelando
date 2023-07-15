590c44a09f9b5706f7890d800108a02d74b37ede
Leonardo Carrera da Silva <59487403+LeonardoCarreraSilva@users.noreply.github.com>
2023-07-15 09:16:40 -0300

    Criando componentes tipografia e campo texto

---
+++ src/componentes/CampoTexto/CampoTexto.jsx
@@ -0,0 +1,36 @@
+import styled from "@emotion/styled"
+
+const LabelEstilizada = styled.label`
+    display: block;
+    width: 100%;
+    box-sizing: border-box;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    `
+    const InputEstilizado = styled.input`
+    display: block;
+    width: 100%;
+    box-sizing: border-box;
+    margin-top: ${props => props.theme.espacementos.xs};
+
+    border-radius: ${props => props.theme.espacementos.s};
+    border: 1px solid;
+    border-color: ${props => props.theme.cores.neutras.a};
+    background: ${props => props.theme.cores.branco};
+    height: 40px;
+
+    font-weight: 400;
+    font-size: 14px;
+    font-height: 17px;
+`
+
+
+
+export const CampoTexto = ({titulo}) =>{
+    return(<LabelEstilizada>
+        {titulo}
+        <InputEstilizado />
+    </LabelEstilizada>)
+}
---
+++ src/componentes/Tipografia/Tipografia.jsx
@@ -0,0 +1,66 @@
+import styled from "@emotion/styled"
+
+const componentes = {
+    h1: 'h1',
+    body: 'p',
+}
+
+const estilos ={
+    h1: `
+        font-weight: 600;
+        fons-size: 40px;
+        line-height: 49px;
+    `,
+    h2: `
+        font-weight: 400;
+        line-height: normal;
+        font-size: 16px;
+    `,
+    h3:`
+        font-weight: 400;
+        line-height: normal;
+        font-size: 16px;
+    `,
+    body: `
+        font-weight: 400;
+        fons-size: 20px;
+        line-height: 24px;
+    `,
+    body1:`
+        font-weight: 400;
+        fons-size: 16px;
+        line-height: normal;
+    `,
+    body1Bold:`
+        font-weight: 700;
+        fons-size: 16px;
+        line-height: normal;
+    `,
+    body2:`
+        font-weight: 400;
+        fons-size: 16px;
+        line-height: normal;
+    `,
+    body2Bold:`
+        font-weight: 700;
+        fons-size: 16px;
+        line-height: normal;
+    `,
+    subtitle1:`
+        font-weight: 400;
+        fons-size: 14px;
+        line-height: normal;
+    `,
+    subtitle2:`
+        font-weight: 400;
+        fons-size: 14px;
+        line-height: normal;
+        text-decoration-line: underline;
+    `,
+}
+
+export const Tipografia = ({ variante, componente, children }) => {
+    const tag = componentes[componente]
+    const ComponenteUtilizado = styled[tag] `${estilos[variante]}`
+    return (<ComponenteUtilizado>{children}</ComponenteUtilizado>)
+}
