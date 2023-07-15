d493e811d8d41765e0e5ebc78a91a31d27eecd8d
Leonardo Carrera da Silva <59487403+LeonardoCarreraSilva@users.noreply.github.com>
2023-07-15 08:34:00 -0300

    Ajustes no tema global

--- src/componentes/ProvedorTema/ProvedorTema.jsx
+++ src/componentes/ProvedorTema/ProvedorTema.jsx
@@ -33,7 +33,8 @@ const tema ={
         l: '32px',
         xl: '48px',
         xxl: '64px'
-    }
+    },
+    fonteFamily: "'Montserrat', sans-serif"
 }
 
 export const ProvedorTema= ({ children }) =>{
